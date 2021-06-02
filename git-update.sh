6d9bd32a4e296e4bf71552bddb496a181f481c38
Daft Penguin <daftpenguin@gmail.com>
2021-06-02 07:57:17 +0000

    git change tracking not working like other scripts

--- git-update.sh
+++ git-update.sh
@@ -2,7 +2,7 @@
 
 d=`date +%s`
 
-untracked=`git ls-files --other --exclude-standard --directory | egrep -v '/$' | wc -l`
+untracked=`git ls-files -m | egrep -v '/$' | wc -l`
 if (( untracked > 0)); then
     git add --all
     git commit -m "Latest maps.json for ${d}"
